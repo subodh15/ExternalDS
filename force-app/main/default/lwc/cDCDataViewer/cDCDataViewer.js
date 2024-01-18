f29277a7873b786fb9a854f2a350e58f74b72299
subodh15 <subodh15@users.noreply.github.com>
2024-01-18 13:13:37 -0500

    Made some more changes to the LWC

--- force-app/main/default/lwc/cDCDataViewer/cDCDataViewer.js
+++ force-app/main/default/lwc/cDCDataViewer/cDCDataViewer.js
@@ -3,9 +3,18 @@ import { LightningElement, wire, track } from 'lwc';
 import getJsonData from '@salesforce/apex/CDC_DataViewer_Controller.getJsonData';
 
 const columns = [
-    { label: 'Jurisdiction', fieldName: 'wwtp_jurisdiction', type: 'text' },
+    { label: 'State', fieldName: 'wwtp_jurisdiction', type: 'text' },
+    { label: 'County', fieldName: 'county_names', type: 'text' },
+    { label: 'Detection Level', fieldName: 'percentile', type: 'text' },
+    { label: 'Proportion Detected', fieldName: 'detect_prop_15d', type: 'text' },
+
+    
     { label: 'WWTP ID', fieldName: 'wwtp_id', type: 'text' },
-    { label: 'Reporting Jurisdiction', fieldName: 'reporting_jurisdiction', type: 'text' },
+  
+    { label: 'Reporting Jurisdiction', fieldName: 'reporting_jurisdiction', type: 'text' }
+
+
+    
     // Add more columns as needed
 ];
 
